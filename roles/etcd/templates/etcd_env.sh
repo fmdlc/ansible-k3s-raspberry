36a6b0ab1cabdab700d944e69f32ad819852b1be
Facu de la Cruz <fmdlc.unix@gmail.com>
2021-08-07 21:21:01 -0300

    add CA / Fixing different issues and improving style

---
+++ roles/etcd/templates/etcd_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Usage: $: source ./etcd_env.sh
+
+export ETCDCTL_API=3
+export ETCDCTL_CACERT="{{ etcd.initial_ca_dir }}/ca.pem"
+export ETCDCTL_CERT="{{ etcd.initial_ca_dir }}/{{ inventory_hostname.split('.') | first }}.pem"
+export ETCDCTL_KEY="{{ etcd.initial_ca_dir }}/{{ inventory_hostname.split('.') | first }}-key.pem"
+export ETCDCTL_ENDPOINTS="https://node-1:2379,https://node-2:2379,https://node-3:2379"
